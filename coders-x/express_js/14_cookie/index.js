1d96bcacf1a2ccdb19e1a1f542bf3abfba7aab1f
tuongnguyen <tuongnguyencontact@gmail.com>
2019-12-14 22:37:25 +0700

    add 14_cookie

---
+++ coders-x/express_js/14_cookie/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var cookieParser = require('cookie-parser');
+const port = 3000;
+
+const app = express();
+app.use(cookieParser())
+
+app.get('/', (req, res, next) => {
+  res.cookie('cookie', 'demo-cookie');
+  res.send('Hello');
+});
+
+app.get('/cookie', (req, res, next) => {
+  res.send(req.cookies);
+});
+
+
+app.listen(port, () => {
+  console.log('app listening on port '+ port);
+});
